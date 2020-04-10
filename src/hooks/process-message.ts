9a29aba47c4d9fec5a8951f3a36823317856b101
Nuno Barreto <nbarreto@gmail.com>
2020-04-10 12:08:45 +0200

    Add process-message hook

--- src/hooks/process-message.ts
+++ src/hooks/process-message.ts
@@ -2,32 +2,32 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
 
-export default (options = {}): Hook => {
+export default () : Hook => {
   return async (context: HookContext) => {
-    // Get `app`, `method`, `params` and `result` from the hook context
-    const { app, method, result, params } = context;
-    // Function that adds the user to a single message object
-    const addUser = async (message: any) => {
-      // Get the user based on their id, pass the `params` along so
-      // that we get a safe version of the user data
-      const user = await app.service('users').get(message.userId, params);
+    const { data } = context;
 
-      // Merge the message content to include the 'user' object
-      return (
-        ...message,
-        user
-      );
-    };
-
-    // In a find method we need to process the entire page
-    if (method === 'find') {
-      // Map all data to include the `user` information
-      context.result.data = await Promise.all(result.data.map(addUser));
-    } else {
-      // Otherwise just update the single result
-      context.result = await addUser(result);
+    // Throw an error if we didn't get a text
+    if(!data.text) {
+      throw new Error('A message must have a text');
     }
 
+    // The authenticated user
+    const user = context.params.user;
+    // The actual message text
+    const text = data.text
+      // Messages can't be longer than 400 characters
+      .substring(0, 400);
+
+    // Override the original data (so that people can't submit additional stuff)
+    context.data = {
+      text,
+      // Set the user id
+      userId: user._id,
+      // Add the current date
+      createdAt: new Date().getTime()
+    };
+
+    // Best practice: hooks should always return the context
     return context;
   };
 }
