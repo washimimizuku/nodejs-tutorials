af544e2df7fb6c49adaae72737a8b9463d26b42e
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-11-20 17:12:27 +0000

    Add password hashing when creating/updating User

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services/password';
+
 // An interface that describes the properties
 // that are required to create a new User
 interface UserAttrs {
@@ -30,6 +32,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
