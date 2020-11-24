2604a84e1c2cd7595d29eb38b57cb636061e1319
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-11-24 16:43:53 +0000

    Add basic tests for signup route

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -6,6 +6,8 @@ import { app } from '../app';
 let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
+    process.env.JWT_KEY = 'some_jwt_key';
+
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
