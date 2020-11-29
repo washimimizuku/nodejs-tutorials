e9c18944520d8c81038ad3c05e3003c0fb478f84
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-11-29 12:42:05 +0000

    Call page getInitialProps from the main App Component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default function _app({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
+const appComponent = function _app({ Component, pageProps, currentUser }) {
+    return (
+        <div>
+            <h1>Header {currentUser.email}</h1>
+            <Component {...pageProps} />
+        </div>
+    );
+};
+
+appComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentuser');
+
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    console.log(pageProps);
+
+    return {
+        pageProps,
+        ...data,
+    };
+};
+
+export default appComponent;
