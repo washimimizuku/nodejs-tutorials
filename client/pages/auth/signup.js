d8d087b5e119446fffbd24df7cb6f47b2da153c1
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-11-29 09:25:09 +0000

    Redirect to homepage when signup is successful

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 
 import useRequest from '../../hooks/use-request';
 
@@ -13,6 +13,7 @@ export default function signupPage() {
             email,
             password,
         },
+        onSuccess: () => Router.push('/'),
     });
 
     const onSubmit = async (event) => {
