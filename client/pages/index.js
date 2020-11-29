51cbb9ff1c9d87f29c9f6038401091710b787b87
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-11-29 12:15:19 +0000

    Create helper for using axios on getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,16 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const landingPage = ({ currentUser }) => {
     console.log(currentUser);
     return <h1>Landing Page</h1>;
 };
 
-landingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined') {
-        // we are on the server!
-        // requests should be made to http://ingress-nginx.ingress-nginx...
-        const { data } = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-            {
-                headers: req.headers,
-            }
-        );
+landingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
 
-        return data;
-    } else {
-        // we are on the browser!
-        // requests can be made with a base url of ''
-        const { data } = await axios.get('/api/users/currentuser');
+    const { data } = await client.get('/api/users/currentuser');
 
-        return data;
-    }
+    return data;
 };
 
 export default landingPage;
