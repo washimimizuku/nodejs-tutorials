233168a0840afbb97e0ebc0f0c72ab1d882d218b
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-11-29 10:19:42 +0000

    Set initial props for index file on the client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,29 @@
-const landingPage = () => {
+import axios from 'axios';
+
+const landingPage = ({ currentUser }) => {
+    console.log(currentUser);
     return <h1>Landing Page</h1>;
+};
+
+landingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        // we are on the server!
+        // requests should be made to http://ingress-nginx.ingress-nginx...
+        const { data } = await axios.get(
+            'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: req.headers,
+            }
+        );
+
+        return data;
+    } else {
+        // we are on the browser!
+        // requests can be made with a base url of ''
+        const { data } = await axios.get('/api/users/currentuser');
+
+        return data;
+    }
 };
 
 export default landingPage;
