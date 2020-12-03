3ccfab3b9804d2cec1c55abe3a0d970b2711f2db
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-03 11:36:47 +0000

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Re-export code from errors
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection.error';
@@ -5,6 +6,7 @@ export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 
+// Re-export code from middlewares
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
