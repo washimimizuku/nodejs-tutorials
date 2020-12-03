4a0f8ea92b1e16db998e7dfcf359d41aa15b2430
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-03 11:35:33 +0000

    Start using ticketing common package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,22 +2,21 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@washimimizuku/ticketing-common';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV !== 'test',
-    })
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
 );
 
 app.use(currentUserRouter);
@@ -26,7 +25,7 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 app.get('*', async () => {
-    throw new NotFoundError();
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,51 +1,53 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import {
+  validationRequest,
+  BadRequestError,
+} from '@washimimizuku/ticketing-common';
 
-import { validationRequest } from '../middlewares/validate-request';
 import { User } from '../models/User';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
-    '/api/users/signup',
-    [
-        body('email').isEmail().withMessage('Email must be provided'),
-        body('password')
-            .trim()
-            .isLength({ min: 4, max: 20 })
-            .withMessage('Password must be between 4 and 20 characters'),
-    ],
-    validationRequest,
-    async (req: Request, res: Response) => {
-        const { email, password } = req.body;
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be provided'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  validationRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-        const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-        if (existingUser) {
-            throw new BadRequestError('Email in use.');
-        }
-
-        const user = User.build({ email, password });
-        await user.save();
-
-        // Generate JWT
-        const userJwt = jwt.sign(
-            {
-                id: user.id,
-                email: user.email,
-            },
-            process.env.JWT_KEY!
-        );
-
-        // Store it on the session object
-        req.session = {
-            jwt: userJwt,
-        };
-
-        return res.status(201).send(user);
+    if (existingUser) {
+      throw new BadRequestError('Email in use.');
     }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on the session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    return res.status(201).send(user);
+  }
 );
 
 export { router as signupRouter };
