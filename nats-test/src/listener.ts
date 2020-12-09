fb97011401911d4b848a1562243f56d03084bc2a
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-09 21:25:54 +0100

    Fix indentation

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -4,35 +4,33 @@ import { randomBytes } from 'crypto';
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-    url: 'http://localhost:4222',
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-    console.log('Listener connected to NATS');
+  console.log('Listener connected to NATS');
 
-    stan.on('close', () => {
-        console.log('NATS connection closed!');
-        process.exit();
-    });
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
 
-    const options = stan.subscriptionOptions().setManualAckMode(true);
-    const subscription = stan.subscribe(
-        'ticket:created',
-        'orders-service-queue-group',
-        options
-    );
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable();
 
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
+  const subscription = stan.subscribe('ticket:created', options);
 
-        if (typeof data === 'string') {
-            console.log(
-                `Received event #${msg.getSequence()} with data: ${data}`
-            );
-        }
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
 
-        msg.ack();
-    });
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()} with data: ${data}`);
+    }
+
+    msg.ack();
+  });
 });
 
 process.on('SIGINT', () => stan.close());
