72d9a9e0180414ebf3059cddd2ada43a2d9fb219
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-09 22:08:58 +0100

    Move listener classes to the events folder

---
+++ nats-test/src/events/TicketCreatedListener.ts
@@ -0,0 +1,14 @@
+import { Message, Stan } from 'node-nats-streaming';
+
+import { Listener } from './Listener';
+
+export class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+
+    msg.ack();
+  }
+}
