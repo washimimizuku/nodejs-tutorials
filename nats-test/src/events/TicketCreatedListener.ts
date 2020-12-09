b329c2ae219bc35edcd521e5be410d0fa27e3534
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-09 22:36:10 +0100

    Add verification for types of listeners

--- nats-test/src/events/TicketCreatedListener.ts
+++ nats-test/src/events/TicketCreatedListener.ts
@@ -1,13 +1,19 @@
-import { Message, Stan } from 'node-nats-streaming';
+import { Message } from 'node-nats-streaming';
 
 import { Listener } from './Listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './TicketCreatedEvent';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
+
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
