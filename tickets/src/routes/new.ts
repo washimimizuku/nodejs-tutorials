dbe10869f7c04ad74c739df1aaab7d5366fe5c62
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-10 16:27:28 +0100

    Add NATS connection through a wrapper

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -6,6 +6,8 @@ import {
 } from '@washimimizuku/ticketing-common';
 
 import { Ticket } from '../models/Ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -29,6 +31,14 @@ router.post(
     });
 
     await ticket.save();
+
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
+
     res.status(201).send(ticket);
   }
 );
