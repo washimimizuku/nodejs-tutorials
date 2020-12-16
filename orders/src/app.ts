c1b0b65df354c4df9ae15d409dd7c67ca2d1aa84
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-16 16:23:09 +0100

    Scaffold route handlers for orders

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -8,12 +8,10 @@ import {
   currentUser,
 } from '@washimimizuku/ticketing-common';
 
-/*
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
-*/
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes/index';
+import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 app.set('trust proxy', true);
@@ -26,12 +24,11 @@ app.use(
 );
 
 app.use(currentUser);
-/*
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
-*/
+
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.get('*', async () => {
   throw new NotFoundError();
