e878a7ca1b10f8972b5cfbbf9d079f7e5a1eeb82
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-16 19:42:44 +0100

    Add tests for the new endpoint

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,88 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
+import { cookieHelper } from '../../test/cookie-helper';
+
+it('has a route handler listening to /api/orders for post requests', async () => {
+  const response = await request(app).post('/api/orders').send({});
+
+  expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if the user is signed in', async () => {
+  const response = await request(app).post('/api/orders').send({});
+
+  expect(response.status).toEqual(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+  const cookie = await cookieHelper('test@test.com');
+
+  const response = await request(app)
+    .post('/api/orders')
+    .set('Cookie', cookie)
+    .send({});
+
+  expect(response.status).not.toEqual(401);
+});
+
+it('returns an error if the ticket does not exist', async () => {
+  const cookie = await cookieHelper('test@test.com');
+  const ticketId = mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', cookie)
+    .send({
+      ticketId: ticketId,
+    })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  const cookie = await cookieHelper('test@test.com');
+
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'SOMN_USER_ID',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', cookie)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  const cookie = await cookieHelper('test@test.com');
+
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', cookie)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+});
