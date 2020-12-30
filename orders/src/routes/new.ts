066c17f56fd46034cd8d7f93c81a57b39c3eb0d2
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-30 15:26:55 +0000

    Add versioning to database records

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -12,6 +12,7 @@ import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
 import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 import { natsWrapper } from '../nats-wrapper';
+import { TicketUpdatedListener } from '../events/listeners/ticket-updated-listener';
 
 const router = express.Router();
 
