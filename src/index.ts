2edffd956681ee67e8aa4045ce92b966a6b9879b
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-30 11:13:11 +0000

    Add events for creating and cancelling orders

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/types/order-status';
+export * from './events/order-created-event';
+export * from './events/order-cancelled-event';
 
 // Re-export code from middlewares
 export * from './middlewares/current-user';
