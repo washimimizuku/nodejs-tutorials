abecd2ec1904d7c1f94b94019ccb7344f222bb11
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-05 20:36:11 +0000

    Create payments service

---
+++ payments/src/test/cookie-helper.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+export const cookieHelper = async (email: string) => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    email: email,
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string with the cookie with the encoded data
+  return [`express:sess=${base64}`];
+};
