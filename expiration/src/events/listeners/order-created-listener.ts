f8fafdc1341dfc2b211347ab0bd2c01f1944a578
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-05 17:53:26 +0000

    Complete order created listener for expiration service

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,15 +1,29 @@
 import {
   Listener,
-  OrderCancelledEvent,
   OrderCreatedEvent,
   Subjects,
 } from '@washimimizuku/ticketing-common';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('Waiting this many milliseconds to process the job:', delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
+
+    msg.ack();
+  }
 }
