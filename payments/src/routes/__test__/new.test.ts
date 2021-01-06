b63e346fa355dca349215070abe5e1f5ff7559d7
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-06 12:54:45 +0000

    Test stripe using a mock

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,9 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { cookieHelper } from '../../test/cookie-helper';
+import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   const cookie = await cookieHelper('test@test.com');
@@ -61,4 +64,32 @@ it('returns a 400 when purchasing a cancelled order', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const cookie = await cookieHelper('test@test.com', userId);
+
+  const order = await Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', cookie)
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual('eur');
 });
