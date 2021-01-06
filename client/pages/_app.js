bd1d34d4a8d0911fee7f58348d4fd1b06b74324d
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-06 16:40:11 +0000

    Create the new ticket route on the client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,9 @@ const AppComponent = function _app({ Component, pageProps, currentUser }) {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component currentUser={currentUser} {...pageProps} />
+      <div className="container">
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 };
