2d70774168fd47049a7e3b487101aa622393cd0b
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-18 23:02:51 +0200

    Add posts module

--- nestjs-blog-api/src/app.module.ts
+++ nestjs-blog-api/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
