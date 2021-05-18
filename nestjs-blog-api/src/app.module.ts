8b516932220757e4d5cd03ea391b071784b9fb5a
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-18 22:46:48 +0200

    Add auth module

--- nestjs-blog-api/src/app.module.ts
+++ nestjs-blog-api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
