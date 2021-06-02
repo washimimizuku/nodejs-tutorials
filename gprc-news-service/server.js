cc213c63406cd4ca2ab76cca76b84ccf74095113
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-06-02 22:32:48 +0100

    Create get news method

--- gprc-news-service/server.js
+++ gprc-news-service/server.js
@@ -16,7 +16,7 @@ const newsProto = grpc.loadPackageDefinition(packageDefinition);
 
 const server = new grpc.Server();
 
-const news = [
+let news = [
   {
     id: '1',
     title: 'Note 1',
@@ -34,6 +34,12 @@ const news = [
 server.addService(newsProto.NewsService.service, {
   getAllNews: (_, callback) => {
     callback(null, news);
+  },
+  getNews: (_, callback) => {
+    const newsId = _.request.id;
+    const newsItem = news.find(({ id }) => newsId == id);
+
+    callback(null, newsItem);
   },
   addNews: (call, callback) => {
     const _news = { id: Date.now(), ...call.request };
